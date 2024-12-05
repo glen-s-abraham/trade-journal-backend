6ae09df89aeb5a0d99f5492bc65a78314716003f
iss211 <glen.abraham@immcossi.com>
2024-12-05 08:50:29 +0530

    initial commit

---
+++ src/main/java/com/trading/tradejournal/TradejournalApplication.java
@@ -0,0 +1,13 @@
+package com.trading.tradejournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradejournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradejournalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trading/tradejournal/TradejournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trading.tradejournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradejournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
